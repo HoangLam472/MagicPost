d80af30650f32edb441d0a83ffc84bb56d7dd99d
Nam <nammai6969@gmail.com>
2023-12-29 21:09:26 +0700

    thong ke

--- client/js/admin.js
+++ client/js/admin.js
@@ -2,6 +2,7 @@ document.getElementById("list__1--title").addEventListener('click',function(){
     var table2 = document.getElementById("table2")
     table2.classList.remove("none");
     document.getElementById("add--refresh").classList.remove("none");
+    document.getElementById("piechart").classList.add("none");
 });
 
 document.getElementById("btn__add-admin").addEventListener('click',function(){
@@ -21,5 +22,35 @@ document.getElementById("form__close--admin").addEventListener('click',function(
   });
   document.getElementById("icon__refresh").addEventListener('click',function(){
     location.reload();
-  
+    // document.getElementById("piechart").classList.add("none");
   });
+
+  document.getElementById("list__2--title").addEventListener('click',function(){
+    document.getElementById("piechart").classList.remove("none");
+    var table2 = document.getElementById("table2")
+    table2.classList.add("none");
+    document.getElementById("add--refresh").classList.add("none");
+  });
+
+
+  google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Thất bại',     50],
+          ['Đã giao',      250],
+          ['Thành công',  300],
+          
+        ]);
+
+        var options = {
+          title: 'Thống kê tình trạng giao hàng'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
