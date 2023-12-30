ada43193a261a31a11e459efcddce7af8400921f
HoangLam472 <83564729+HoangLam472@users.noreply.github.com>
2023-12-31 02:27:37 +0700

    nam xanh occho

--- client/config.js
+++ client/config.js
@@ -1,6 +1,6 @@
 const config = {
-    // API_URL: 'http://localhost:8080',
-    API_URL: 'https://hl-web.onrender.com'
+    API_URL: 'http://localhost:8080',
+    // API_URL: 'https://hl-web.onrender.com'
 }
 
 export {config}
